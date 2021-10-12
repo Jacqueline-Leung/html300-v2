155848edca86a76d11bb2e97c6034a035649e6dd
Jacqueline Leung <jacqgreer@yahoo.com>
2021-10-11 21:06:04 -0700

    assignment07

---
+++ lesson07/assignment/vue-work/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/Recipes',
+    name: 'Recipes',
+    // route level code-splitting
+   
+    component: () => import(/* webpackChunkName: "about" */ '../views/Recipes.vue')
+  },
+  {
+    path: '/Nutritions',
+    name: 'Nutritions',
+    // route level code-splitting
+    
+    component: () => import('../views/Nutritions.vue')
+  },
+  {
+    path: '/MyCookbook',
+    name: 'MyCookbook',
+    
+    component: () => import('../views/MyCookbook.vue')
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
